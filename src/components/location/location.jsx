0b19accfbe43da9ac8b90955690bbf9eb614c2c8
Sriram <sriramsubramanian23@gmail.com>
2023-08-15 14:06:34 +0530

    Fixed flag api and Header section

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -1,18 +1,37 @@
-import React from "react"
-import moment from "moment"
-import "./location.scss"
-const Location = ({city, country, date}) => {
-    const FLAG = `https://sriram-23.vercel.app/flag?q=${country}`
-    console.log(FLAG)
-    return (
-        <section className="location-section">
-            <div className="location-subsection">
-                <h1>{city && city}</h1>
-                <figure className="flag">{country && <img crossorigin="anonymous" src={FLAG} alt="Flag"/>}</figure>
-            </div>
-            <span>Last Updated: {date && moment.unix(date).fromNow()}</span>  
-        </section>
-    )
-}
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import "./location.scss";
+import axios from "axios";
 
-export default Location
+const Location = ({ city, country, date }) => {
+  const FLAG = `https://sriram-23.vercel.app/flag?q=${country}`;
+
+  const [b64, setB64] = useState("");
+  useEffect(() => {
+    console.log("Inside useEffect");
+    axios.get(FLAG).then((res) => {
+      if (res.data) {
+        setB64(res.data);
+      }
+    });
+    console.log("b64: " + b64);
+  }, [country]);
+  //   console.log(B64);
+  return (
+    <section className="location-section">
+      <span>Last Updated: {date && moment.unix(date).fromNow()}</span>
+      <div className="location-container">
+        <div className="location-subsection">
+          <h1>{city && city}</h1>
+          {b64 && (
+            <figure className="flag">
+              {country && <img src={b64} alt={country + "Flag"} />}
+            </figure>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Location;
