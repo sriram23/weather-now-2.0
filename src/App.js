f54a6a07ff27f5816d14b47afb4181806eb576b8
Sriram <sriramsubramanian23@gmail.com>
2023-08-15 14:14:10 +0530

    Fixed styling of main container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Weather from "./components/weather/weather.jsx";
 
 function App() {
-  return <Weather />;
+  return <div className="main-container"><Weather /></div>;
 }
 
 export default App;
